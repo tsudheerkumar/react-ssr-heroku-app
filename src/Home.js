187b9fcfb13d55feeb5d68445f0dec174e4af08c
Tumarada <stumarada@dxc.com>
2021-01-20 16:17:40 +0530

    fixed filter issue

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React ,{useEffect, useState} from 'react';
 import Flight from './Flight';
 import RadioButton from './RadioButton';
+import axios from 'axios';
 //import "./index.css";
 export default props => {
-    const [isLaunched, setIsLaunched] = React.useState("No");
-    const [isLanded, setIsLanded] = React.useState("No");
+    const [flightData, setFlightData] = useState(props.flights);
+    const [isLaunched, setIsLaunched] = useState("No");
+    const [isLanded, setIsLanded] = useState("No");
     const launchChangeHandler = (event) => {
         setIsLaunched(event.target.value);
     }
     const landChangeHandler = (event) => {
         setIsLanded(event.target.value);
     }
+    useEffect(() => {
+        fetchFlights(getUrl());
+      }, [isLaunched,isLanded]);
+    const getUrl = () => {
+        let baseUrl = "https://api.spaceXdata.com/v3/launches?limit=100";
+        let finalUrl ='';
+        if (isLaunched=="Yes"&& isLanded=="Yes") {
+            finalUrl=baseUrl+"&launch_success=true&land_success=true";
+        }else if(isLaunched=="Yes"&& isLanded=="No"){
+            finalUrl=baseUrl+"&launch_success=true";
+        }else if(isLaunched=="No"&& isLanded=="Yes"){
+            finalUrl=baseUrl+"&land_success=true";
+        }else{
+            finalUrl=baseUrl;
+        }
+        return finalUrl;
+    }
+    const fetchFlights =(url) => {  
+        axios.get(url)
+            .then(response => {
+                setFlightData(response.data);
+            }) 
+      };
+    
     return <>
         <header>SpacEx Launch Programs</header>
         <div className="row">
@@ -36,14 +62,14 @@ export default props => {
                     <div className="flight-launch">
                         <h5 className="flight-launch-title row">Successful Launch</h5>
                         <RadioButton 
-                        changed={ launchChangeHandler } 
+                        changed={launchChangeHandler} 
                         id="1" 
                         isSelected={ isLaunched === "Yes" } 
                         label="True" 
                         value="Yes" 
                     />
                         <RadioButton 
-                        changed={ launchChangeHandler } 
+                        changed={launchChangeHandler} 
                         id="2" 
                         isSelected={ isLaunched === "No" } 
                         label="False" 
@@ -53,14 +79,14 @@ export default props => {
                     <div className="flight-land">
                         <h5 className="flight-landtitle">Successful Land</h5>
                         <RadioButton 
-                        changed={ landChangeHandler } 
+                        changed={landChangeHandler} 
                         id="3" 
                         isSelected={ isLanded === "Yes" } 
                         label="True" 
                         value="Yes" 
                     />
                         <RadioButton 
-                        changed={ landChangeHandler } 
+                        changed={landChangeHandler} 
                         id="4" 
                         isSelected={ isLanded === "No" } 
                         label="False" 
@@ -69,7 +95,7 @@ export default props => {
                     </div>
                 </div>
               <div className="flight-container col-s-12 col-m-9 col-l-10 "> 
-            {props.flights && props.flights.map(flight=>(<Flight flight={flight}></Flight>))}
+            {flightData && flightData.map(flight=>(<Flight flight={flight}></Flight>))}
                
             </div>
     </div>
