9f59a67d41910cd6724e00de3b35ff15d6ee5301
Tumarada <stumarada@dxc.com>
2021-01-21 12:49:19 +0530

    fixed tab  issue

--- src/RadioButton.js
+++ src/RadioButton.js
@@ -4,7 +4,7 @@ import "./RadioButton.css";
 const RadioButton = (props) => {
     return (
         <div className="radio-toolbar col-s-3 col-m-3 col-l-3" aria-label={props.value}>
-            <input name={props.label} onKeyPress={props.changed} id={props.id} onChange={props.changed} aria-label={props.value} value={props.value} type="radio" checked={props.isSelected} />
+            <input name={props.label+props.id} onKeyPress={props.changed} id={props.id} onChange={props.changed} aria-label={props.value} value={props.value} type="radio" checked={props.isSelected} />
             <label htmlFor={props.id}>{props.label}</label>
         </div>
     );
